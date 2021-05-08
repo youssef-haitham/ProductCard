63d66ab2212d64b4bac451243cf6b65c33005c9b
youssef-haitham <youssefhaitham1@hotmail.com>
2021-05-08 06:20:00 +0200

    Adding shimmer loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'product';
-  product = {
-    title: 'Product Title Goes Here',
-    description: 'Product Description Goes Here',
-    price: 25.5,
-    discountPrice: 20.5,
-    imgSRC: "../assets/catafast.png"
-  };
+  product = null;
+
+  ngOnInit() {
+    this.onViewDidLoad();
+  }
+
+  onViewDidLoad(){
+    setTimeout(() => {
+        this.product = {
+          title: 'Product Title Goes Here',
+          description: 'Product Description Goes Here',
+          price: 25.5,
+          discountPrice: 20.5,
+          imgSRC: "../assets/catafast.png"
+        };
+      
+    }, 3000);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
